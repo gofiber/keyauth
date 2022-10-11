c11c2678e233a96cdc6de4a86480b0e55f382337
Juan Calderon-Perez <835733+gaby@users.noreply.github.com>
2022-10-10 20:09:06 -0400

    Add simple test

--- main_test.go
+++ main_test.go
@@ -3,3 +3,27 @@
 // 📝 Github Repository: https://github.com/gofiber/fiber
 
 package keyauth
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/utils"
+)
+
+func Test_Default(t *testing.T) {
+	app := fiber.New()
+
+	app.Use(New(keyauth.Config{
+		KeyLookup: "cookie:access_token",
+		ContextKey: "my_token",
+	}))
+	  
+	app.Get("/", func(c *fiber.Ctx) error {
+		token, _ := c.Locals("my_token").(string)
+		return c.SendString(token)
+	})
+
+	resp, err := app.Test(httptest.NewRequest("GET", "/", ErrMissingOrMalformedAPIKey))
+}
