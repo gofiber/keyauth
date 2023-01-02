425c89ca9561555430b44bae4c6d4ddcf36fca18
DavZim <david-zimmermann@hotmail.de>
2023-01-02 12:51:31 +0100

    restructure nested tests to use subtests

--- main_test.go
+++ main_test.go
@@ -87,110 +87,36 @@ func TestAuthSources(t *testing.T) {
 
 	var CorrectKey = "specials: !$%,.#\"!?~`<>@$^*(){}[]|/\\123"
 	// define test cases
+	testSources := []string {"header", "cookie", "query", "param", "form"}
+
 	tests := []struct {
-		route 		  string
-		authSource    string
+		route         string
 		authTokenName string
 		description   string
 		APIKey        string
 		expectedCode  int
 		expectedBody  string
 	}{
-		// header:access_token auth
 		{
 			route:         "/",
-			authSource:    "header",
 			authTokenName: "access_token",
-			description:   "Testing Header:access_token",
+			description:   "auth with correct key",
 			APIKey:        CorrectKey,
 			expectedCode:  200,
 			expectedBody:  "Success!",
 		},
 		{
 			route:         "/",
-			authSource:    "header",
 			authTokenName: "access_token",
-			description:   "Testing Header:access_token with a wrong key",
-			APIKey:        "WRONGKEY",
-			expectedCode:  401,
+			description:   "auth with no key",
+			APIKey:        "",
+			expectedCode:  401, // 404 in case of param authentication
 			expectedBody:  "missing or malformed API Key",
 		},
-
-		// cookie:access_token auth
 		{
 			route:         "/",
-			authSource:    "cookie",
 			authTokenName: "access_token",
-			description:   "Testing cookie:access_token",
-			APIKey:        CorrectKey,
-			expectedCode:  200,
-			expectedBody:  "Success!",
-		},
-		{
-			route:         "/",
-			authSource:    "cookie",
-			authTokenName: "access_token",
-			description:   "Testing cookie:access_token with a wrong key",
-			APIKey:        "WRONGKEY",
-			expectedCode:  401,
-			expectedBody:  "missing or malformed API Key",
-		},
-		
-		// query:access_token auth
-		{
-			route:         "/",
-			authSource:    "query",
-			authTokenName: "access_token",
-			description:   "Testing query:access_token",
-			APIKey:        CorrectKey,
-			expectedCode:  200,
-			expectedBody:  "Success!",
-		},
-		{
-			route:         "/",
-			authSource:    "query",
-			authTokenName: "access_token",
-			description:   "Testing query:access_token with a wrong key",
-			APIKey:        "WRONGKEY",
-			expectedCode:  401,
-			expectedBody:  "missing or malformed API Key",
-		},
-
-		// param:access_token auth
-		{
-			route:         "/key/", // will end as '/key/:access_token'
-			authSource:    "param",
-			authTokenName: "access_token",
-			description:   "Testing param:access_token",
-			APIKey:        CorrectKey,
-			expectedCode:  200,
-			expectedBody:  "Success!",
-		},
-		{
-			route:         "/key/",
-			authSource:    "param",
-			authTokenName: "access_token",
-			description:   "Testing param:access_token with a wrong key",
-			APIKey:        "WRONGKEY",
-			expectedCode:  401,
-			expectedBody:  "missing or malformed API Key",
-		},
-		
-		// form:access_token auth
-		{
-			route:         "/",
-			authSource:    "form",
-			authTokenName: "access_token",
-			description:   "Testing form:access_token",
-			APIKey:        CorrectKey,
-			expectedCode:  200,
-			expectedBody:  "Success!",
-		},
-		{
-			route:         "/",
-			authSource:    "form",
-			authTokenName: "access_token",
-			description:   "Testing form:access_token with a wrong key",
+			description:   "auth with wrong key",
 			APIKey:        "WRONGKEY",
 			expectedCode:  401,
 			expectedBody:  "missing or malformed API Key",
@@ -198,73 +124,84 @@ func TestAuthSources(t *testing.T) {
 	}
 
 
-	for _, test := range tests {
-		// setup the fiber endpoint
-		// note that if UnescapePath: false (the default)
-		// escaped characters (such as `\"`) will not be handled correctly in the tests
-		app := fiber.New(fiber.Config{UnescapePath: true})
-		authMiddleware := New(Config{
-			KeyLookup:  test.authSource + ":" + test.authTokenName,
-			Validator:  func(c *fiber.Ctx, key string) (bool, error) {
-				if key == CorrectKey {
-					return true, nil
+	for _, authSource := range testSources {
+		t.Run(authSource, func(t *testing.T) {
+			for _, test := range tests {
+				// setup the fiber endpoint
+				// note that if UnescapePath: false (the default)
+				// escaped characters (such as `\"`) will not be handled correctly in the tests
+				app := fiber.New(fiber.Config{UnescapePath: true})
+
+				authMiddleware := New(Config{
+					KeyLookup:  authSource + ":" + test.authTokenName,
+					Validator:  func(c *fiber.Ctx, key string) (bool, error) {
+						if key == CorrectKey {
+							return true, nil
+						}
+						return false, ErrMissingOrMalformedAPIKey
+					},
+				})
+
+				var route string
+				if authSource == "param" {
+					route = test.route + ":" + test.authTokenName
+					app.Use(route, authMiddleware)
+				} else {
+					route = test.route
+					app.Use(authMiddleware)
 				}
-				return false, ErrMissingOrMalformedAPIKey
-			},
+
+				app.Get(route, func(c *fiber.Ctx) error {
+					return c.SendString("Success!")
+				})
+
+				// construct the test HTTP request
+				var req *http.Request
+				req, _ = http.NewRequest("GET", test.route, nil)
+				
+				// setup the apikey for the different auth schemes
+				if authSource == "header" {
+
+					req.Header.Set(test.authTokenName, test.APIKey)
+
+				} else if authSource == "cookie" {
+
+					req.Header.Set("Cookie", test.authTokenName + "=" + test.APIKey)
+
+				} else if authSource == "query" || authSource == "form" {
+
+					q := req.URL.Query()
+					q.Add(test.authTokenName, test.APIKey)
+					req.URL.RawQuery = q.Encode()
+
+				} else if authSource == "param" {
+
+					r := req.URL.Path
+					r = r + url.PathEscape(test.APIKey)
+					req.URL.Path = r
+
+				}
+
+				res, err := app.Test(req, -1)
+
+				utils.AssertEqual(t, nil, err, test.description)
+
+				// test the body of the request
+				body, err := ioutil.ReadAll(res.Body)
+				// for param authentication, the route would be /:access_token 
+				// when the access_token is empty, it leads to a 404 (not found)
+				// not a 401 (auth error)
+				if authSource == "param" && test.APIKey == "" {
+					test.expectedCode = 404
+					test.expectedBody = "Cannot GET /"
+				}
+				utils.AssertEqual(t, test.expectedCode, res.StatusCode, test.description)
+
+				// body
+				utils.AssertEqual(t, nil, err, test.description)
+				utils.AssertEqual(t, test.expectedBody, string(body), test.description)
+			}
 		})
-
-		var route string
-		if test.authSource == "param" {
-			route = test.route + ":" + test.authTokenName
-			app.Use(route, authMiddleware)
-		} else {
-			route = test.route
-			app.Use(authMiddleware)
-		}
-
-		app.Get(route, func(c *fiber.Ctx) error {
-			return c.SendString("Success!")
-		})
-
-		
-		// construct the test HTTP request
-		var req *http.Request
-		req, _ = http.NewRequest("GET", test.route, nil)
-		
-		// setup the apikey for the different auth schemes
-		if test.authSource == "header" {
-
-			req.Header.Set(test.authTokenName, test.APIKey)
-
-		} else if test.authSource == "cookie" {
-			
-			req.Header.Set("Cookie", test.authTokenName + "=" + test.APIKey)
-
-		} else if test.authSource == "query" || test.authSource == "form" {
-			
-			q := req.URL.Query()
-			q.Add(test.authTokenName, test.APIKey)
-			req.URL.RawQuery = q.Encode()
-
-		} else if test.authSource == "param" {
-			
-			r := req.URL.Path
-			r = r + url.PathEscape(test.APIKey)
-			req.URL.Path = r
-
-		}
-
-		res, err := app.Test(req, -1)
-
-		utils.AssertEqual(t, nil, err, test.description)
-
-		// test the body of the request
-		body, err := ioutil.ReadAll(res.Body)
-		utils.AssertEqual(t, test.expectedCode, res.StatusCode, test.description)
-
-		// body
-		utils.AssertEqual(t, nil, err, test.description)
-		utils.AssertEqual(t, test.expectedBody, string(body), test.description)
 	}
 }
 
